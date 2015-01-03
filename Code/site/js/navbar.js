32dff2a49aed72e84de467e17466248b5f25b6d7
CharlesBean <beanchar@msu.edu>
2015-01-02 21:21:21 -0500

    Init

---
+++ Code/site/js/navbar.js
@@ -0,0 +1,45 @@
+(function(){
+    
+    var button = document.getElementById("cn-button"),
+        wrapper = document.getElementById('cn-wrapper'),
+        overlay = document.getElementById('cn-overlay');
+    
+    var open = false; // Open flag
+    
+    button.addEventListener('click', navHandler, false);
+    //button.addEventListener('focus', navHandler, false);
+    wrapper.addEventListener('click', cnHandler, false);
+    
+    function cnHandler(e){
+        e.stopPropagation();   
+    }
+    
+    function navHandler(e){
+        if (!e) var e = window.event;
+        e.stopPropagation(); // No click event trigger on document (preventDefault??)
+        
+        if (!open){
+            openNav();
+        }
+        else{
+            closeNav();
+        }
+    }
+    
+    function openNav(){
+        open = true;
+        button.innerHTML = "Close";
+        overlay.classList.add('on-overlay');
+        wrapper.classList.add('opened-nav');
+    }
+    
+    function closeNav(){
+        open = false;
+        button.innerHTML = "Nav";
+        overlay.classList.remove('on-overlay');
+        wrapper.classList.remove('opened-nav');
+    }
+    
+    document.addEventListener('click', closeNav);
+    
+})();
